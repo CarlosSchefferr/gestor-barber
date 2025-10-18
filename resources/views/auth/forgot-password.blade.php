51df98f4c37faf0aae3c795a7e7fde2a0fdce911
Carlos Augusto <94874330+CarlosSchefferr@users.noreply.github.com>
2025-10-18 16:29:03 -0300

    novos ajustes

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,4 +1,9 @@
 <x-guest-layout>
+       <div class="text-center mb-6">
+            <!-- Logo -->
+            <img src="{{ asset('images/logo.png') }}" alt="Logo" class="mx-auto h-20 w-auto mb-2">
+
+        </div>
     <div class="mb-4 text-sm text-gray-600">
         {{ __('Esqueceu sua senha? Sem problemas. Basta informar seu endereço de e-mail e nós enviaremos um link para redefinir sua senha.') }}
     </div>
@@ -16,10 +21,15 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-center mt-4">
             <x-primary-button>
                 {{ __('Enviar link de redefinição de senha') }}
             </x-primary-button>
         </div>
+          <div class="mt-4 text-center text-sm text-gray-600">
+                    <a href="{{ route('login') }}" class="text-barber-500 hover:underline font-semibold">
+                        {{ __('Voltar') }}
+                    </a>
+                </div>
     </form>
 </x-guest-layout>
